29a2fae95a1c030dc149046415d7c65d45b4cc28
esposen <ep2851@bard.edu>
2018-02-24 15:16:27 -0500

    linking csv logger

--- brain_blocks/Assets/Scripts/LoggerCSV.cs
+++ brain_blocks/Assets/Scripts/LoggerCSV.cs
@@ -1,16 +1,99 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Text;
+using System.IO;
+using System;
 
-public class LoggerCSV : MonoBehaviour {
+public class LoggerCSV : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public static LoggerCSV instance = null;
+
+	private List<string[]> rows = new List<string[]>();
+
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			this.CreateTitles();
+		}
+		else if (instance != this)
+		{
+			Destroy(this);
+		}
+
 	}
-	
+
+
+	public static LoggerCSV GetInstance()
+	{
+		return instance;
+	}
+
+	private void CreateTitles()
+	{
+		string[] titles = {"Control Mode", "Time Stamp", "Event", "Event Value" };
+		rows.Add(titles);
+	}
+
+	public void AddEventLog(int mode, float time, string event_log, float event_val)
+	{
+        string[] toAdd = { mode.ToString(), time.ToString(), event_log, event_val.ToString() };
+		rows.Add(toAdd);
+	}
+
+	public void PrintLogger()
+	{
+		for (int i = 0; i < rows.Count; i++)
+		{
+			string[] r = rows[i];
+			string toPrint = "";
+			for (int j = 0; j < r.Length; j++)
+			{
+				toPrint += r[j] + "    ";
+			}
+			Debug.Log("Row " + i.ToString() + ": " + toPrint);
+		}
+	}
+	public void SaveCSV()
+	{
+		Debug.Log("Saving CSV");
+		string[][] output = new string[rows.Count][];
+		for (int i = 0; i < output.Length; i++)
+		{
+			output[i] = rows[i];
+		}
+		int len = output.GetLength(0);
+		string divider = ",";
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int index = 0; index < len; index++)
+			sb.AppendLine(string.Join(divider, output[index]));
+
+
+		string filePath = getPath();
+
+		StreamWriter outStream = System.IO.File.CreateText(filePath);
+		outStream.WriteLine(sb);
+		outStream.Close();
+	}
+
+	// Following method is used to retrive the relative path as device platform
+	private string getPath()
+	{
+#if UNITY_EDITOR
+		return Application.dataPath + "/CSV/" + "event_logger.csv";
+#else
+                return Application.dataPath +"/"+"Saved_data.csv";
+#endif
+	}
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 }
