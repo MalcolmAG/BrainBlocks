d1b136fc53e9ab25ab8783e286790e95a7240cc6
esposen <ep2851@bard.edu>
2018-03-12 20:16:51 -0400

    training

--- brain_blocks/Assets/Scripts/MentalCommandControl.cs
+++ brain_blocks/Assets/Scripts/MentalCommandControl.cs
@@ -4,12 +4,14 @@ using UnityEngine.UI;
 using TMPro;
 public class MentalCommandControl : MonoBehaviour {
 
-    public Button btnNeutral, btnLeft, btnRight;
+    public Button btnNeutral, btnLeft, btnRight,
+                  btnNeutralClear, btnLeftClear,
+                  btnRightClear;
     public Slider slider;
     public TextMeshProUGUI trainPercentage;
     public TextMeshProUGUI curAction;
     public TextMeshProUGUI status;
-
+    public bool print;
     uint userId;
     bool training = false;
     string trainType;
@@ -22,20 +24,16 @@ public class MentalCommandControl : MonoBehaviour {
         bindEvents();
 	}
 
-    IEnumerator UpdateSlider(){
-        while (true){
-            slider.value += Time.deltaTime;
-            int percent = (int)(100 * (slider.value / 8));
-            trainPercentage.text = percent.ToString() + "%";
-            if (percent == 100) break;
-            else yield return null;
+    private void Update()
+    {
+        if(print){
+            Debug.Log("LEFT:" + EmoMentalCommand.MentalCommandActionPower[5]);
+            Debug.Log("RIGHT:" + EmoMentalCommand.MentalCommandActionPower[6]);
         }
-        yield return new WaitForSeconds(1f);
-        slider.value = 0;
-        trainPercentage.text = "0%";
-        btnLeft.gameObject.SetActive(true);
-        btnRight.gameObject.SetActive(true);
     }
+
+    //------------------------------Emotiv Event Functions------------------------------//
+
 
     void bindEvents(){
         engine.UserAdded += onUserAdded;
@@ -53,7 +51,7 @@ public class MentalCommandControl : MonoBehaviour {
             return;
         }
         EdkDll.IEE_MentalCommandAction_t action = args.emoState.MentalCommandGetCurrentAction();
-        Debug.Log("current action: " + action.ToString());
+        //Debug.Log("current action: " + action.ToString());
         switch (action)
         {
             case EdkDll.IEE_MentalCommandAction_t.MC_NEUTRAL:
@@ -62,7 +60,7 @@ public class MentalCommandControl : MonoBehaviour {
                 break;
             case EdkDll.IEE_MentalCommandAction_t.MC_RIGHT:
                 cube.action = 1;
-                curAction.text = "CurrentAction: Pushing";
+                curAction.text = "CurrentAction: Right";
                 break;
             case EdkDll.IEE_MentalCommandAction_t.MC_LEFT:
                 cube.action = 2;
@@ -82,9 +80,6 @@ public class MentalCommandControl : MonoBehaviour {
     void onTrainingStarted(object sender, EmoEngineEventArgs args)
     {
         status.text = "Training " + trainType;
-        btnNeutral.gameObject.SetActive(false);
-        btnLeft.gameObject.SetActive(false);
-        btnLeft.gameObject.SetActive(false);
         training = true;
     }
 
@@ -92,16 +87,71 @@ public class MentalCommandControl : MonoBehaviour {
         status.text = "Success! Training " + trainType +" Concluded";
         //should confirm user that they accept that training, in this case auto accept
         engine.MentalCommandSetTrainingControl(userId, EdkDll.IEE_MentalCommandTrainingControl_t.MC_ACCEPT);
+        for (int i = 0; i < EmoMentalCommand.MentalCommandActionsEnabled.Length; i ++){
+            Debug.Log("LIST " + i+ ": " + EmoMentalCommand.MentalCommandActionsEnabled[i]);
+		}
     }
 
     void onTrainingCompleted(object sender, EmoEngineEventArgs args){
         training = false;
+        UpdateButtons();
+        cube.action = cube.ACTION_RESET;
     }
-	// Update is called once per frame
-	void Update () {
-	
+
+//------------------------------UI Helper Functions------------------------------//
+	//Coroutine to control Training_Slider
+    //Takes 8 seconds to get to 100%
+    IEnumerator UpdateSlider()
+	{
+		while (true)
+		{
+			slider.value += Time.deltaTime;
+			int percent = (int)(100 * (slider.value / 8));
+			trainPercentage.text = percent.ToString() + "%";
+			if (percent == 100) break;
+			else yield return null;
+		}
+		yield return new WaitForSeconds(1f);
+		slider.value = 0;
+		trainPercentage.text = "0%";
+
 	}
 
+    //Activates/Deactivates Buttons depending on user interaction
+    void UpdateButtons () {
+        switch(trainType){
+            case "Neutral":
+                btnNeutralClear.interactable = true;
+                btnLeft.interactable = true;
+                btnRight.interactable = true;
+				break;
+            case "clear neutral":
+                btnNeutralClear.interactable = false;
+                btnRight.interactable = false;
+                btnRightClear.interactable = false;
+                btnLeft.interactable = false;
+                btnLeftClear.interactable = false;
+                break;
+            case "Right":
+                btnRightClear.interactable = true;
+                break;
+            case "clear right":
+                btnRightClear.interactable = false;
+                break;
+            case "Left":
+                btnLeftClear.interactable = true;
+                break;
+            case "clear left":
+                btnLeftClear.interactable = false;
+                break;
+
+		}
+	}
+
+//------------------------------UI OnClick Functions------------------------------//
+
+
+    //Called by Neutral_Button
     public void TrainNeutral(){
         training = true;
         trainType = "Neutral";
@@ -113,6 +163,8 @@ public class MentalCommandControl : MonoBehaviour {
 		engine.MentalCommandSetTrainingAction(userId, EdkDll.IEE_MentalCommandAction_t.MC_NEUTRAL);
 		engine.MentalCommandSetTrainingControl(userId, EdkDll.IEE_MentalCommandTrainingControl_t.MC_START);
 	}
+
+    //Called by Left_Button
 	public void TrainLeft()
 	{
 		training = true;
@@ -125,6 +177,8 @@ public class MentalCommandControl : MonoBehaviour {
 		engine.MentalCommandSetTrainingAction(userId, EdkDll.IEE_MentalCommandAction_t.MC_LEFT);
 		engine.MentalCommandSetTrainingControl(userId, EdkDll.IEE_MentalCommandTrainingControl_t.MC_START);
 	}
+
+    //Called by Right_Button
     public void TrainRight(){
         training = true;
         trainType = "Right";
@@ -133,8 +187,30 @@ public class MentalCommandControl : MonoBehaviour {
 		StartCoroutine(UpdateSlider());
 
 		engine.MentalCommandSetActiveActions(userId, (uint)EdkDll.IEE_MentalCommandAction_t.MC_RIGHT);
-        engine.MentalCommandSetTrainingAction(userId, EdkDll.IEE_MentalCommandAction_t.MC_RIGHT);
-        engine.MentalCommandSetTrainingControl(userId, EdkDll.IEE_MentalCommandTrainingControl_t.MC_START);
+		engine.MentalCommandSetTrainingAction(userId, EdkDll.IEE_MentalCommandAction_t.MC_RIGHT);
+		engine.MentalCommandSetTrainingControl(userId, EdkDll.IEE_MentalCommandTrainingControl_t.MC_START);
+	}
+
+	//Called by Clear_Neutral_Button, Clear_Right_Button & Clear_Left_Button
+	public void ClearTraining(string type){
+
+        EdkDll.IEE_MentalCommandAction_t action = EdkDll.IEE_MentalCommandAction_t.MC_NEUTRAL;
+        trainType = "clear neutral";
+        switch(type){
+            case "Left":
+                action = EdkDll.IEE_MentalCommandAction_t.MC_LEFT;
+                trainType = "clear left";
+				break;
+            case "Right":
+                action = EdkDll.IEE_MentalCommandAction_t.MC_RIGHT;
+				trainType = "clear right";
+				break;
+        }
+
+        EdkDll.IEE_MentalCommandSetTrainingAction((uint)EmoUserManagement.currentUser, action);
+		EdkDll.IEE_MentalCommandSetTrainingControl((uint)EmoUserManagement.currentUser, EdkDll.IEE_MentalCommandTrainingControl_t.MC_ERASE);
+        UpdateButtons();
+        status.text= "Cleared " + type + " Training Data";
     }
 
 }
--- brain_blocks/Assets/Scripts/cube.cs
+++ brain_blocks/Assets/Scripts/cube.cs
@@ -4,34 +4,34 @@ using System.Collections;
 public class cube : MonoBehaviour {
 
     public static int action = 0;
-    public static float speed = 7.0f / 8.0f;
+    public static float speed = .2f;
+    public static readonly int ACTION_RESET = -1;
     public static readonly int ACTION_NEUTRAL = 0;
     public static readonly int ACTION_RIGHT = 1;
     public static readonly int ACTION_LEFT = 2;
 	// Use this for initialization
-	void Start () {
+    public static void ResetPos(){
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {   
+	//Controls animation of BCI training Block
+	void Update () {
+        if(action == ACTION_RESET){
+            transform.position = new Vector3(4, 15, 0);
+            action = ACTION_NEUTRAL;
+        }
         //Neutral
         if (action == ACTION_NEUTRAL)
         {
-            if (transform.localPosition.x > 4 )
-				transform.Translate(0, 0, -speed * Time.deltaTime * 10);
-            else if(transform.localPosition.x < 4)
-				transform.Translate(0, 0, speed * Time.deltaTime * 10);
-		}
+            if (transform.localPosition.x > 4.3)
+                transform.Translate(-speed * Time.deltaTime * 10, 0, 0);
+            else if (transform.localPosition.x < 3.8)
+                transform.Translate(speed * Time.deltaTime * 10, 0, 0);
+        }
         //Right
         else if(action == ACTION_RIGHT){
-			if (transform.localPosition.x < 7)
+			if (transform.localPosition.x < 15)
 			{
 				transform.Translate(speed * Time.deltaTime * 10, 0, 0);
-			}
-			else
-			{
-				transform.localPosition = new Vector3(4, 0, 0);
 			}
         }
         //Left
@@ -40,10 +40,6 @@ public class cube : MonoBehaviour {
 			{
 				transform.Translate( -speed * Time.deltaTime * 10, 0, 0);
 			}
-			else
-			{ 
-				transform.localPosition = new Vector3(4, 0, 0);
-			}
         }
-	}
+    }
 }
